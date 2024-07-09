89f6e19e3a484cf06fbaef6c5fb81d78861c4e60
unknown <viktoralmeida25@gmail.com>
2024-07-08 22:13:28 -0300

    add stack-brackets.cpp

---
+++ stack-brackets.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+	stack<char> brackets;
+	string str;
+	string open_brackets = "[{(";
+	string close_brackets = "]})";
+	cout << "Enter the string"<<endl;
+	cin >> str;
+	bool isValid = true;
+
+	for (char &s: str){
+		// If the char is ), ] or }
+		if(close_brackets.find(s) != string::npos){
+			if(brackets.size() == 0){
+				isValid = false;
+				break;
+			}
+			// Check if the close bracket is valid
+			if(close_brackets.find(s) != open_brackets.find(brackets.top())){
+				isValid = false;
+				break;
+			}
+				brackets.pop();
+		}
+		// Case (, [ or {
+		else{
+			brackets.push(s);
+		}
+
+	}
+
+	
+	if(brackets.size()>0){
+		isValid = false;
+	}
+
+	cout<< isValid<<endl;
+
+	return 0;
+}
+
